492bedaa13ec9e16eab41388954dbbe7ecffc079
Hugo Lopes <hugolope30@icloud.com>
2023-09-24 14:36:50 -0300

    Key Operation

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
-function clickButtonPom() {
-    document.querySelector('#som_tecla_pom').play(); 
+function playSound(idSound) {
+    document.querySelector(idSound).play();
 }
 
-document.querySelectorAll('.tecla');
+const keyList = document.querySelectorAll('.tecla');
 
+let counter = 0;
 
+// Enquanto
+while(counter < keyList.length) {
 
+    const key = keyList[counter];
+    const instrument = key.classList[1];
+    const sound = `#som_${instrument}`;
 
-
-
+    key.onclick = function () {
+        playSound(sound);
+    }
+    counter += 1;
+} 
