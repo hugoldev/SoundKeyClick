25f53a6988765a974aeb13e9316923b9ee026250
Hugo Lopes <hugolope30@icloud.com>
2023-09-25 00:17:17 -0300

    Refactoring

--- main.js
+++ main.js
@@ -4,27 +4,47 @@ function playSound(idSound) {
 
 const keyList = document.querySelectorAll('.tecla');
 
-for(let counter = 0; counter < keyList.length; counter++) {
+// for(let counter = 0; counter < keyList.length; counter++) {
 
-    const key = keyList[counter];
+//     const key = keyList[counter];
+//     const instrument = key.classList[1];
+//     const sound = `#som_${instrument}`;
+
+//     key.onclick = function () {
+//         playSound(sound);
+//     } 
+
+//     key.onkeydown = function() {
+//         if(event.code === "Enter" || event.code === "Space") {
+//             key.classList.add('ativa');
+//         }
+//     }
+
+//     key.onkeyup = function() {
+//         key.classList.remove('ativa');
+//     }
+
+// }
+
+keyList.forEach((key) => {
     const instrument = key.classList[1];
     const sound = `#som_${instrument}`;
 
-    key.onclick = function () {
+    key.onclick = () => {
         playSound(sound);
     } 
 
-    key.onkeydown = function() {
+    key.onkeydown = () => {
         if(event.code === "Enter" || event.code === "Space") {
             key.classList.add('ativa');
         }
     }
 
-    key.onkeyup = function() {
+    key.onkeyup = () => {
         key.classList.remove('ativa');
     }
+})
 
-}
 
 
 // let counter = 0;
